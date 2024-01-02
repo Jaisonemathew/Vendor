b6f8fd3957285f622462ba6580e1899c6a625505
Jaison E Mathew <jaisonemathew14@gmail.com>
2024-01-02 13:04:23 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express  = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
+const path = require('path');
 const app = express()
 app.use(cors())
 app.use(express.json())
